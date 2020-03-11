fde7e0d089c1c55850481a805cbb7937437f34f9
KonstantinasFalkovskis <integro@inbox.lt>
2020-03-11 17:18:32 +0200

    selenium grid is modified: status checker is added

--- healthcheck.sh
+++ healthcheck.sh
@@ -1,24 +1,24 @@
 #!/usr/bin/env bash
 # Environment Variables
 #=========
-# hub
-# port
+# HUB_HOST
+# PORT
 # environment
 # platform
 # browserName
-# module
+# MODULE
 
-# echo "Checking if hub is ready - $hub"
+# echo "Checking if " + "- $HUB_HOST " + "is ready"
 echo "Checking if hub is ready"
-# while [ "$( curl -s http://$hub:port/wd/hub/status | jq -r .value.ready )" != "true" ]
+# while [ "$( curl -s http://$HUB_HOST:$PORT/wd/hub/status | jq -r .value.ready )" != "true" ]
 while [ "$( curl -s http://192.168.0.106:4444/wd/hub/status | jq -r .value.ready )" != "true" ]
 do
 	sleep 1
 done
 
 # start the java command
-java -cp Airbnb-java-selenium-testng-build.jar:Airbnb-java-selenium-testng-build-sources.jar: \
-Airbnb-java-selenium-testng-build-tests.jar:libs/* \
+java -cp selenium-docker.jar:selenium-docker-sources.jar:selenium-docker-tests \
+selenium-docker-tests.jar:libs/* \
    # -Dhub=$hub \
    # -Dport=$port \
    # -Denvironment=$environment \  #need to investigate is Local testing needed or not. If not remove ENV -param
